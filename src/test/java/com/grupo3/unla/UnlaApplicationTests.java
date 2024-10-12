45f3bb8d7b78ffb41edd66cf9de63685972e24d0
Sergio Robles <mpthrash@gmail.com>
2024-10-12 18:38:27 -0300

    Update UnlaApplicationTests.java

--- src/test/java/com/grupo3/unla/UnlaApplicationTests.java
+++ src/test/java/com/grupo3/unla/UnlaApplicationTests.java
@@ -2,6 +2,7 @@ package com.grupo3.unla;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = UnlaApplication.class)
 @ActiveProfiles("test")
